7c5cee66702b08967325ca8f420bfc2581cc2ea4
vmuchui <kaigongimuchui@gmail.com>
2019-07-22 10:27:39 +0300

    add intents and toasts for onClicklistener on both buttons in the activity.

--- app/src/main/java/com/example/beertracker/MainActivity.java
+++ app/src/main/java/com/example/beertracker/MainActivity.java
@@ -2,13 +2,45 @@ package com.example.beertracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.addBeers)
+    Button mAddBeers;
+
+    Button mFindBreweries;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFindBreweries = findViewById(R.id.findBreweries);
+        mFindBreweries.setOnClickListener(this::onClick);
+        ButterKnife.bind(this);
+
+        mAddBeers.setOnClickListener(this);
+
+
     }
+    @Override
+    public void onClick(View view) {
+        if (view==mAddBeers) {
+            Toast.makeText(MainActivity.this,"Add beers you've consumed...",Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(MainActivity.this,AddBeerActivity.class);
+            startActivity(intent);
+        }if (view==mFindBreweries){
+            Toast.makeText(MainActivity.this,"Find Breweries near you...",Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(MainActivity.this,FindBreweriesActivity.class);
+            startActivity(intent);
+        }
+
+    }
+
 }
