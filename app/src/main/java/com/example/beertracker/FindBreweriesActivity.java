6386c86c8d10813837e652be175899aaed9cbbb7
vmuchui <kaigongimuchui@gmail.com>
2019-07-22 10:25:16 +0300

    help users find breweries around them.

--- app/src/main/java/com/example/beertracker/FindBreweriesActivity.java
+++ app/src/main/java/com/example/beertracker/FindBreweriesActivity.java
@@ -2,13 +2,26 @@ package com.example.beertracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class FindBreweriesActivity extends AppCompatActivity {
+
+    @BindView(R.id.messageText)
+    TextView mTextmessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_breweries);
+        ButterKnife.bind(this);
+
+        Typeface niconne = Typeface.createFromAsset(getAssets(),"fonts/Niconne-Regular.ttf");
+        mTextmessage.setTypeface(niconne);
+
     }
 }
