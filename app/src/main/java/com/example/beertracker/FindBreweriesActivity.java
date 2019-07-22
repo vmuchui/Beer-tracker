33e02c0ad92b350ab5b6f828d89a9a1327555205
vmuchui <kaigongimuchui@gmail.com>
2019-07-22 10:18:25 +0300

    add fonts to be used. add activity to record beers consumed. add activity to help user find breweries around them. add buttons to start add beer activity and find breweries.

---
+++ app/src/main/java/com/example/beertracker/FindBreweriesActivity.java
@@ -0,0 +1,14 @@
+package com.example.beertracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FindBreweriesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_breweries);
+    }
+}
